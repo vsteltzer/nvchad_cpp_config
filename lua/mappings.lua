3dc5d3c6ee0a4331db08c14295d8564e25d03b00
Valentin <v.steltzer@infors-ht.com>
2024-08-15 16:01:24 +0200

    Add dap, dapui and mappings.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,4 +9,21 @@ map("n", "<leader>cb", ":CMakeBuild<cr>", { desc = "Call CMakeBuild" })
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+
+-- Nvim DAP
+map("n", "<Leader>dl", "<cmd>lua require'dap'.step_into()<CR>", { desc = "Debugger step into" })
+map("n", "<Leader>dj", "<cmd>lua require'dap'.step_over()<CR>", { desc = "Debugger step over" })
+map("n", "<Leader>dk", "<cmd>lua require'dap'.step_out()<CR>", { desc = "Debugger step out" })
+map("n", "<Leader>d<space>", "<cmd>lua require'dap'.continue()<CR>", { desc = "Debugger continue" })
+map("n", "<Leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = "Debugger toggle breakpoint" })
+map(
+	"n",
+	"<Leader>dd",
+	"<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+	{ desc = "Debugger set conditional breakpoint" }
+)
+map("n", "<Leader>de", "<cmd>lua require'dap'.terminate()<CR>", { desc = "Debugger reset" })
+map("n", "<Leader>dc", "<cmd>lua require'dapui'.close()<CR>", { desc = "Debugger ui close" })
+map("n", "<Leader>dr", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Debugger run last" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
---
+++ lua/plugins/nvim-dap.lua
@@ -0,0 +1,28 @@
+return {
+  	{
+		"mfussenegger/nvim-dap",
+		config = function()
+			require("configs.dap")
+		end,
+	},
+
+  {
+    "nvim-neotest/nvim-nio" 
+  },
+
+	{
+		"rcarriga/nvim-dap-ui",
+		config = function()
+			require("dapui").setup()
+		end,
+		requires = { "mfussenegger/nvim-dap" },
+	},
+
+	{
+		"theHamsta/nvim-dap-virtual-text",
+		config = function()
+			require("nvim-dap-virtual-text").setup()
+		end,
+		requires = { "mfussenegger/nvim-dap" },
+	},
+}
