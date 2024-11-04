382b7e11408d9cfb5b49ae28c0f8367e226797e5
Valentin <v.steltzer@infors-ht.com>
2024-11-04 15:28:15 +0100

    LSP: Add LSP support for null-ls and clangd. Install Mason plugins and treesitter.

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,22 @@
 require "nvchad.options"
 
--- add yours here!
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 16
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "100"
+
+vim.g.cmake_link_compile_commands = 1
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
